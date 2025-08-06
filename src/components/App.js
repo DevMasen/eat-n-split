35b44686d3067512175aa0142ec78e0975928b1f
DevMasen <mohammadwh400@gmail.com>
2025-08-06 13:27:08 +0330

    E98: showAddFriend State and functionality implanted

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../index.css';
 import AddFriendForm from './AddFriendForm';
 import Button from './Button';
@@ -5,12 +6,19 @@ import FriendList from './FriendList';
 import SplitBillForm from './SplitBillForm';
 
 export default function App() {
+	const [showAddFriend, setShowAddFriend] = useState(false);
+
+	function handleShowAddFriend() {
+		setShowAddFriend(curShow => !curShow);
+	}
 	return (
 		<div className="app">
 			<div className="sidebar">
 				<FriendList />
-				<AddFriendForm />
-				<Button>Add Friend</Button>
+				{showAddFriend && <AddFriendForm />}
+				<Button onClick={handleShowAddFriend}>
+					{showAddFriend ? 'close' : 'Add Friend'}
+				</Button>
 			</div>
 			<SplitBillForm />
 		</div>
