68be24d55bc24e4364d2967c77833c110800dc8d
DevMasen <mohammadwh400@gmail.com>
2026-02-26 14:26:47 +0330

    fix: clear fake data

--- src/components/App.js
+++ src/components/App.js
@@ -5,30 +5,9 @@ import Button from './Button';
 import FriendList from './FriendList';
 import SplitBillForm from './SplitBillForm';
 
-const initialFriends = [
-	{
-		id: 118836,
-		name: 'Clark',
-		image: 'https://i.pravatar.cc/48?u=118836',
-		balance: -7,
-	},
-	{
-		id: 933372,
-		name: 'Sarah',
-		image: 'https://i.pravatar.cc/48?u=933372',
-		balance: 20,
-	},
-	{
-		id: 499476,
-		name: 'Anthony',
-		image: 'https://i.pravatar.cc/48?u=499476',
-		balance: 0,
-	},
-];
-
 export default function App() {
 	const [showAddFriend, setShowAddFriend] = useState(false);
-	const [friendList, setFriendList] = useState(initialFriends);
+	const [friendList, setFriendList] = useState([]);
 	const [selectedFriend, setSelectedFriend] = useState(null);
 
 	function handleShowAddFriend() {
@@ -47,8 +26,8 @@ export default function App() {
 			curList.map(friend =>
 				friend.id === selectedFriend.id
 					? { ...friend, balance: friend.balance + value }
-					: friend
-			)
+					: friend,
+			),
 		);
 		setSelectedFriend(null);
 	}
